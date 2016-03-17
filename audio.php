f0f2d1e88633fd1e54ad0daf0bf786ef9244677e
PierreDG <pierre.de-gourcy@hetic.net>
2016-03-17 17:24:06 +0100

    tentative audio

---
+++ audio.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title> AUDIO</title>
+</head>
+<body>
+	
+	<h1>AUDIO</h1>
+
+	
+<embed height="0" width="0" 
+  src="http://youtube.googleapis.com/v/TI79P6_Jl3I&autoplay=1&loop=1" />
+
+</body>
+</html>
