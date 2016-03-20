20ce6d8900aa2d459e0ecc3f8d9e0688c6da3093
PierreDG <pierre.de-gourcy@hetic.net>
2016-03-20 16:55:36 +0100

    UPDATE form PHP

--- contact.php
+++ contact.php
@@ -9,12 +9,13 @@
 
 
 <!doctype html>
-<html>
+<html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<title>FROGGER MD</title>
-		<link rel="stylesheet" href="css/contact.css">
+		<title>FROGGER</title>
+		<link rel="stylesheet" href="src/css/contact2.css">
 
+		<link rel="stylesheet" href="src/css/reset.css">
 		<link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
 		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 		<link rel="icon" href="favicon.ico" type="image/x-icon">
@@ -24,15 +25,15 @@
 		<div class="container">
 			<div class="nav-bar">
 				<div class="logo">
-					<a href="styles.html"><img src="images/logo.png"></a>
+					<a href="#"><img src="images/logo.png"></a>
 				</div>
 
 				<div class="items">
 					<ul>
-						<li id="li4"><h4><a href="styles.html">ACCUEIL</a></h4></li>
-						<li id="li5"><h4><a href="regles.html">RÈGLES</a></h4></li>
-						<li id="li6"><h4><a href="contact.php">CONTACT</a></h4></li>
-						<!-- <li><a href="#" onClick="window.open('pop_up.html','mon_popup','toolbar=1,location=1,directories=1,status=1,scrollbars=1,resizable=1,copyhistory=1,width=800,height=400,screenX=200,screenY=200');">PROFIL</a></li> -->
+						<li><a href="homepage.html">ACCUEIL</a></li>
+						<li><a href="regles.html">RÈGLES</a></li>
+						<li><a href="contact.html">CONTACT</a></li>
+						<li><a href="#">PROFIL</a></li>
 					</ul>
 				</div>
 			</div>
@@ -44,7 +45,7 @@
 			<span class="horaires">Lundi au Vendredi : 08h30 - 21h30</span>
 		</div>
     <div class="background">
-      <img src="images/work.jpeg" alt="qg"/>
+      <img src="src/images/work.jpeg" alt="qg"/>
     </div>
 
     <div class="container2">
@@ -55,18 +56,18 @@
     	</div>
     	<div class="team">
     		<div class="rang-1">
-    			<img src="images/gala.png">
-    			<img src="images/chassin.png">
-    			<img src="images/pdg.png">
+    			<img class="gala" src="src/équipes/gala.png">
+    			<img class="arthur" src="src/équipes/chassin.png">
+    			<img class="pierre" src="src/équipes/pdg.png">
     		</div>
     		<div class="description-1">
     			<p>Gala Pillot<br><span class="green">Développeur back</span></p>
     			<p>Arthur Chassin<br><span class="green">Designer</span></p>
-    			<p>Pierre de Gourcy<br><span class="green">Chef de Projet<br>Developpeur back</span></p>
+    			<p>Pierre de Gourcy<br><span class="green">Developpeur back</span></p>
     		</div>
     		<div class="rang-2">
-    			<img src="images/peltre.png">
-    			<img src="images/dlr.png">
+    			<img src="src/équipes/peltre.png">
+    			<img src="src/équipes/dlr.png">
     		</div>
     		<div class="description-2">
     			<p>Antoine Peltre<br><span class="green">Développeur front</span></p>
@@ -81,10 +82,10 @@
 			<div class="line">
 			</div>
 			<div class="rs">
-				<a href="#"><img src="images/facebook.png"></a>
-				<a href="#"><img src="images/twitter.png"></a>
-				<a href="#"><img src="images/snapchat.png"></a>
-				<a href="#"><img src="images/instagram.png"></a>
+				<a href="#"><img src="src/images/facebook.png"></a>
+				<a href="#"><img src="src/images/twitter.png"></a>
+				<a href="#"><img src="src/images/snapchat.png"></a>
+				<a href="#"><img src="src/images/instagram.png"></a>
 			</div>
 		</div>
 
@@ -95,6 +96,7 @@
 			<div class="line">
 			</div>
 			<form action="#" method="post">
+				<h3> Vous </h3>
 				<div class="infos">
 					<div class="error">
 						<input type="text" name="name" id="name" placeholder="Votre nom">
@@ -104,20 +106,22 @@
 						<input type="text" name="mail" id="mail" placeholder="Votre adresse mail">
 						<label for="mail"></label>
 					</div>
-					<div class="title">
+					<h3> Votre musique </h3>
+					<div class="titleform">
 						<input type="text" name="title" id="title" placeholder="Titre de la musique">
 						<label for="title"></label>
 					</div>
 					<div class="artist">
-						<input type="artist" name="artist" id="artist" placeholder="Artiste de la musique">
+						<input type="text" name="artist" id="artist" placeholder="Artiste de la musique">
 						<label for="artist"></label>
 					</div>
 					<div class="id">
-						<input type="id" name="id" id="id" placeholder="ID Youtube">
+						<p>Correspond à la valeur après le watch?v=</p>
+						<input type="text" name="id" id="id" placeholder="ID Youtube - exemple : MB6vHZBHH5g">
 						<label for="id"></label>
 					</div>
 					<div class="cover">
-						<input type="cover" name="cover" id="cover" placeholder="URL de la cover">
+						<input type="text" name="cover" id="cover" placeholder="URL de la cover">
 						<label for="cover"></label>
 					</div>
 					<div class="comment">
@@ -125,23 +129,24 @@
 						<label for="comment"></label>
 					</div>
 					<div>
-						<input type="submit" class="valider">
+						<input type="submit" class="submit">
 					</div>
 
 				</div>
 				</form>
 				<div class="map">
-					<img src="images/map.png">
+					<img src="src/images/map.png">
 				</div>
 
 
-
-
    		</div>
-			
+
+   		
+
+		
+	
 
 		<script src="src/js/libs/jquery-2.2.0.min.js"></script>
-		<script src="assets/jquery.min.js"></script>
-		<script src="scripts/menu.js"></script>
+		<script src="src/js/app/contact.js"></script>
 	</body>
 </html>
